5acab2acf2908d170ee333c23333e37f2a0943b3
IvanRuizNavarro47 <145495051+IvanRuizNavarro47@users.noreply.github.com>
2025-09-14 17:47:22 +0200

    pagina primer punto

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card-team");
+
+  cards.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const rotateX = ((y / rect.height) - 0.5) * 20; // más visible
+      const rotateY = ((x / rect.width) - 0.5) * -20;
+
+      card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+    card.addEventListener("mouseleave", () => {
+      card.style.transform = "perspective(1000px) rotateX(0deg) rotateY(0deg)";
+    });
+  });
+});
